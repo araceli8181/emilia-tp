6d662279e5ee8585e0e134ea6de749e6334489b2
Araceli Chaves <arace.chaves@gmail.com>
2021-07-26 03:11:33 -0300

    maquetado terminado

---
+++ app.js
@@ -0,0 +1,18 @@
+const express =require('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+
+app.use(express.static('public'));
+
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','home.html')));
+app.get('/about',(req,res) => res.sendFile(path.join(__dirname, 'views','about.html')));
+app.get('/music',(req,res) => res.sendFile(path.join(__dirname, 'views','music.html')));
+app.get('/contact',(req,res) => res.sendFile(path.join(__dirname, 'views','contact.html')));
+
+
+
+
+
+app.listen(port, () => console.log(`servidor corriendo en http://localhost:${port}`))
